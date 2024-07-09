02ab756eb1073e6f181dc1b6a7b35029e1b459e5
Jenny HY Lin <jennyhylin@gmail.com>
2024-07-09 12:31:51 +1200

    Added cyp_drugs.py param info

--- cyp_drugs.py
+++ cyp_drugs.py
@@ -5,8 +5,13 @@ from typing import Optional
 def chembl_drugs(*drug: str, file_name: Optional[str] = None):
 
     """
-    Obtain approved drugs' ChEMBL IDs, generic drug names, max phases and canonical SMILES
+    Obtain approved drugs' ChEMBL IDs, generic drug/small molecule names, max phases and canonical SMILES
     via using drug names only with an option to save dataframe as tsv files
+
+    :param drug: Enter generic drug names in capital letters to search in ChEMBL database e.g. "AMLODIPINE"
+    :param file_name: Enter file name if needed in order to save dataframe as a .tsv file in working directory
+    :return: A dataframe of small molecules/drugs derived from ChEMBL database 
+    along with their ChEMBL IDs, max phases and canonical SMILES
     """
 
     # dedent to remove leading whitespaces from every line
